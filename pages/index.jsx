d03c07046fbfb8ead5a10f6750f7bba1d5c729c5
Tanmay Yadav <ytanmay122005@gmail.com>
2025-07-14 01:06:10 +0530

    index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -32,7 +32,10 @@ const Home = () => {
             exit="hidden"
             className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
           >
-      "I’m someone who believes in turning ideas into reality through thoughtful design and purposeful development. Whether it’s solving real-world problems or creating experiences that matter, I enjoy the process of building from the ground up. I’m constantly learning, evolving, and pushing boundaries to bring imagination closer to execution."
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
+            exercitationem harum, quia nulla temporibus deleniti libero veniam
+            vero beatae numquam ducimus illum ab similique ipsam tempore fugit
+            quod laudantium debitis.
           </motion.p>
 
           {/* btn */}
