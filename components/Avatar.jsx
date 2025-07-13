1e5d91dccfc60e85b1ce8725225dd6517f0e0977
Tanmay Yadav <ytanmay122005@gmail.com>
2025-07-14 01:04:17 +0530

    Avatar.jsx

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -2,17 +2,16 @@ import Image from "next/image";
 
 const Avatar = () => {
   return (
-    <div className="hidden xl:flex xl:max-w-none pointer-events-none select-none w-[737px] h-[678px] translate-x-[-325px]">
+    <div className="hidden xl:flex xl:max-w-none pointer-events-none select-none">
       <Image
         src="/avatar.png"
         alt="avatar"
         width={737}
         height={678}
-        className="translate-z-0 w-full h-full object-cover"
+        className="translate-z-0 w-full h-full"
       />
     </div>
   );
 };
 
 export default Avatar;
-
