9de1239f4dd5b2803f669f563f4a9dfb28100c0a
Tanmay Yadav <ytanmay122005@gmail.com>
2025-07-13 23:19:36 +0530

    index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,9 +8,10 @@ import { fadeIn } from "../variants";
 const Home = () => {
   return (
     <div className="bg-primary/60 h-full">
-      {/* text */}
-      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+      {/* text + image row */}
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10 flex flex-col xl:flex-row items-center xl:items-stretch">
+        {/* left: text */}
+        <div className="flex-1 flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
           <motion.h1
             variants={fadeIn("down", 0.2)}
@@ -48,25 +49,23 @@ const Home = () => {
             <ProjectsBtn />
           </motion.div>
         </div>
-      </div>
-      {/* image */}
-      <div className="w-[1280px] h-full absolute right-0 bottom-0 flex items-center justify-center">
-        {/* bg img */}
-        <div
-          role="img"
-          className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"
-          aria-hidden
-        />
-
-        {/* particles */}
-        <ParticlesContainer />
-
-        {/* avatar image */}
-        <img
-          src="/avatar.png"
-          alt="Avatar"
-          className="absolute bottom-0 right-0 w-[410px] h-[678px] object-cover xl:block hidden"
-        />
+        {/* right: image */}
+        <div className="flex-1 flex items-center justify-center relative xl:pr-16 xl:pb-12">
+          {/* bg img */}
+          <div
+            role="img"
+            className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0 pointer-events-none"
+            aria-hidden
+          />
+          {/* particles */}
+          <ParticlesContainer />
+          {/* avatar image */}
+          <img
+            src="/avatar.png"
+            alt="Avatar"
+            className="relative z-10 w-[350px] h-[500px] object-cover rounded-none shadow-none xl:block hidden"
+          />
+        </div>
       </div>
     </div>
   );
