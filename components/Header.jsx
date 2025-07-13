7f8d34be20d1611604b2f6e179fdc34d1e55c0ce
Tanmay Yadav <ytanmay122005@gmail.com>
2025-07-13 21:21:28 +0530

    Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
   return (
     <header className="absolute z-30 w-full items-center px-16 xl-px-0 xl:h-[90px]">
       <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
-          {/* logo */}
+        {/* logo absolute left */}
+        <div className="fixed top-10 left-10 z-50">
           <Link href="/">
             <Image
               src="/logo.svg"
